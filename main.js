a7205339254d7f5a5ee4009430f72cd96c92edae
João Scloneski <jscloneski.dev@gmail.com>
2023-05-02 21:44:48 -0300

    final

--- main.js
+++ main.js
@@ -2,15 +2,24 @@ const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const fortune = [
+    'O sucesso virá em breve, continue trabalhando duro.',
+    'A sorte estará ao seu lado quando você menos esperar.',
+    'Grandes conquistas exigem grandes esforços.',
+    'O amor está a caminho, mantenha o coração aberto.',
+    'Acredite em si mesmo e em suas habilidades.',
+    'Aproveite as pequenas coisas da vida e você encontrará a felicidade.',
+    'Seu espírito aventureiro o levará a lugares incríveis.',
+    'Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.'
+]
+
 let contadorCliques = 0;
 
 //Eventos
-//btnTry.addEventListener('click', handleTryClick) 
 btnTry.addEventListener('click', function() {
     contadorCliques++;
     if (contadorCliques === 2) {handleTryClick()};
 })
-
 btnReset.addEventListener('click', handleResetClick)
 document.addEventListener('keydown', function(e) {
     if(e.key == 'Enter' && screen2.classList.contains('hide')) {
@@ -19,10 +28,16 @@ document.addEventListener('keydown', function(e) {
     handleResetClick()}
 })
 
-
+//Funções
+function pickFortune() {
+    let allFortunes = fortune.length
+    let randomNumber = Math.floor(Math.random() * allFortunes)
+    screen2.querySelector('h2').innerText = `${fortune[randomNumber]}`
+}
 
 function handleTryClick () {
     toggleScreen()
+    pickFortune()
 }
 
 function handleResetClick() {
